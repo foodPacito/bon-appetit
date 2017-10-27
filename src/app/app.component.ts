d2ed19d4791f08a1964e4da4bd02659eaf9360c9
duhaali <dohaali415@gmail.com>
2017-10-27 18:57:56 +0200

    (edit) location of resturant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { MapPage } from '../pages/map/map';
 
 
 import { SignInPage } from '../pages/sign-in/sign-in';
@@ -15,7 +15,7 @@ import { UserHomePage } from '../pages/user-home/user-home';
 export class MyApp {
 
 
-  rootPage:any = SignInPage;
+  rootPage:any = SignInPage 
 
   constructor(
     platform: Platform,
