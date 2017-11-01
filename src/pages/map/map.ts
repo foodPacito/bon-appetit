fb252b755e6e522a73f7353b5d69fb91ec4529c6
ElectricFiras <ghanem.firas30@gmail.com>
2017-11-01 17:58:36 +0200

    (Edit) Map Page working

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,105 +1,57 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
-import { AngularFireAuth } from 'angularfire2/auth'
-import { Geolocation } from '@ionic-native/geolocation';
-import { AngularFireDatabase } from 'angularfire2/database'
-
-
-declare var google :any;
-
-let position;
+import { Component , ViewChild ,ElementRef } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { Geolocation ,GeolocationOptions ,Geoposition ,PositionError } from '@ionic-native/geolocation'; 
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker
+ } from '@ionic-native/google-maps';
+ import {Platform} from 'ionic-angular';
+ import { IonicPage } from 'ionic-angular';
+ 
+ 
+declare var google;
 @Component({
  selector: 'page-map',
  templateUrl: 'map.html',
 })
+export class MapPage {
+  @ViewChild('map') mapElement: ElementRef;
+  map: GoogleMap;
+  options : GeolocationOptions;
+  currentPos : Geoposition;
+  rest
 
-  export class MapPage {
-   @ViewChild('map') mapElement:ElementRef;
-   map: any;
-   restaurant 
-   restName;
-   locationss
-   location;
-   latitude;
-   longitude;
-
-constructor( 
-public navCtrl: NavController,
-public navParams: NavParams,
- public geolocation: Geolocation,
- private db: AngularFireDatabase) { 
-  this.restName = this.navParams.get('restName')
-  this.latitude=this.navParams.get('location.latitude')
-  this.longitude=this.navParams.get('location.longitude')
-  console.log(this.restName)
-  console.log(this.latitude)
-  console.log(this.latitude)
   
 
-  this.db.list('/restaurants/'+ this.restName +'/available').valueChanges().subscribe( data => {
-    // console.log(data)
-    // console.log('=============================')
-    
-  })
+  constructor(public navCtrl: NavController, 
+    private geolocation : Geolocation, 
+    private googleMaps: GoogleMaps, 
+    private platform: Platform, 
+    public navParams: NavParams) {
+        this.rest = this.navParams.get('rest')
+  }
 
-}  initMap(){
-this.restaurant = this.navParams.get('resturant');
-this.locationss=(this.restaurant.location.latitude);
-console.log(this.restaurant);
-console.log(this.locationss);
+  ngAfterViewInit() {
+    this.platform.ready().then(() => {
+      this.loadMap();
+    });
+  }
 
-this.geolocation.getCurrentPosition().then((position) => {
+   loadMap() {
+    this.map = new google.maps.Map(this.mapElement.nativeElement, {
+      zoom: 19,
+      center: {lat: this.rest.location.latitude , lng: this.rest.location.longitude}
+    });
 
- let location = new google.maps.LatLng(this.restaurant.location.latitude, this.restaurant.location.longitude);
-
-   let mapOptions = {
-     center: location,
-     zoom: 20,
-     mapTypeId: google.maps.MapTypeId.ROADMAP
-    
-   }
-
-   this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-
- });
-
+    var marker = new google.maps.Marker({
+      position: {lat: this.rest.location.latitude , lng: this.rest.location.longitude},
+      map: this.map,
+      title: this.rest.name
+    });
+  }
 }
-
-ionViewDidLoad() {
-   this.initMap();
-    //     ///console.log(this.mapRef);
-    //     this.showMap();
-    //   }
-   console.log('your page ionViewDidLoad');
-}
-
-
-
- addMarker(){ 
-  let marker = new google.maps.Marker({
-    map: this.map,
-    animation: google.maps.Animation.DROP,
-    position: this.map.getCenter()
-  });
-
-
-    let content = "<h4>Information..</h4>";        
- 
-  this.addInfoWindow(marker, content);
- 
-}
-
-addInfoWindow(marker, content){
- 
-  let infoWindow = new google.maps.InfoWindow({
-    content: content
-  });
- 
-  google.maps.event.addListener(marker, 'click', () => {
-    infoWindow.open(this.map, marker);
-  });
- 
-}
-
-}
-
